1251af78b8ea869c8a830dfe93cb42d4c37d5e32
Pranay shukla <pranay.shukla@hiteshi.com>
2024-04-30 18:14:33 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent,title:"HomePage"},
+    {path:"aboutUs", component:AboutUsComponent, title:"About Us"},
+    {path:"**", component:PageNotFoundComponent, title:"Page Not Found"} // wildCard Route 
+];
